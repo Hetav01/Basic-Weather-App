c529359c49e821a7f3667d5fe91f2d179fb230d8
Hetav Patel <hetav2014@gmail.com>
2020-07-20 12:46:27 +0530

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var input = document.querySelector(".text");
+var submit = document.querySelector(".submit");
+var name = document.querySelector(".place");
+var temperature = document.querySelector(".temperature");
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=11b9cea129e1c344129854c681800e36')
+.then(response => response.json())
+.then(data => console.log(data));
