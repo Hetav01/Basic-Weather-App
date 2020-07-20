009ea67e5e129c00ce786183f724753741e66900
Hetav Patel <hetav2014@gmail.com>
2020-07-20 12:50:37 +0530

    d

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var name = document.querySelector(".place");
 var temperature = document.querySelector(".temperature");
 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=11b9cea129e1c344129854c681800e36')
-.then(response => response.json())
-.then(data => console.log(data));
+
+
+submit.addEventListener("click", () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=11b9cea129e1c344129854c681800e36`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+        .catch(error => alert("wrong city name!"));
+});
